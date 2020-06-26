761574c6f76ed21f2e0b63d16f36a0cf295bfbee
Miles-Alford <miles.alford@hackney.gov.uk>
2020-06-26 14:33:22 +0100

    Create Response Object for Post File Endpoint

--- document-api/V1/Boundary/AddFileResponse.cs
+++ document-api/V1/Boundary/AddFileResponse.cs
@@ -1,8 +1,14 @@
 using System;
+using System.Collections.Generic;
+
 namespace document_api.V1.Boundary
 {
     public class AddFileResponse
     {
-        string StatusCode { get; set; }
+        public readonly IList<string> StatusCode;
+
+        public AddFileResponse(IList<string> statuscode) {
+            StatusCode = statuscode;
+        }
     }
 }
