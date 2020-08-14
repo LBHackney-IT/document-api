2350b9cf3a1c907caf1e51b1bba881861aff8fdf
Miles-Alford <43789512+Miles-Alford@users.noreply.github.com>
2020-08-14 20:12:06 +0100

    Add tests for ErrorsResponse class

---
+++ document-api.Tests/V1/Exceptions/ErrorsResponseTests.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using document_api.V1.Exceptions;
+
+namespace document_api.Tests.V1.Exceptions
+{
+    [TestFixture]
+    public class ErrorsResponseTests
+    {
+        [Test]
+        public void When_a_new_ErrorResponse_Object_is_created_its_default_status_parameter_is_fail()
+        {
+            var error = new ErrorsResponse("some kind of error");
+
+            Assert.AreEqual(error.Status, "fail");
+
+        }
+
+        [Test]
+        public void When_a_new_ErrorResponse_object_is_created_it_returns_the_same_error_message()
+        {
+            var errorMessage = "Something Broke";
+
+            var error = new ErrorsResponse(errorMessage);
+
+            Assert.AreEqual(error.Errors, errorMessage);
+        }
+    }
+
+}
